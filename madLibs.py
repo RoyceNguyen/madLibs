0d9be234c90f065581009999552e6adea32d9668
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-03 20:58:04 -0500

    Initial commit

---
+++ madLibs.py
@@ -0,0 +1,11 @@
+#Mad Libs program that reads in text files and lets the user add their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
+
+#TODO: find occurrences and prompt user to replace them
+
+
+
+#TODO: print out text
+
+
+
+#TODO: save result to a new text file
