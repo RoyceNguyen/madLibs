bec875cf25fb70e14758df7ac6ea659a7da044c7
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-03 22:06:51 -0500

    Ask for file name to read and save result to a new text file

--- madLibs.py
+++ madLibs.py
@@ -1,11 +1,12 @@
 #Mad Libs program that reads in text files and lets the user add their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
-
+import os,re
 #TODO: find occurrences and prompt user to replace them
-
-
 
 #TODO: print out text
 
 
 
-#TODO: save result to a new text file
+#TODO: Ask for file name to read and save result to a new text file
+fileName = input('Please enter the name of the template file you wish to use:  ')
+lib = open('{0}/{1}.txt'.format(os.getcwd(), fileName))
+string = lib.read()
