428aef3c8de30d878e8790be0ed9f8afa1ff33c8
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-03 22:14:30 -0500

    finding nouns/verb/adv/adj in the entered string

--- madLibs.py
+++ madLibs.py
@@ -1,12 +1,20 @@
 #Mad Libs program that reads in text files and lets the user add their own text anywhere the word ADJECTIVE, NOUN, ADVERB, or VERB appears in the text file
+
 import os,re
-#TODO: find occurrences and prompt user to replace them
+
+
+# and prompt user to replace them
 
 #TODO: print out text
 
 
 
-#TODO: Ask for file name to read and save result to a new text file
+# Ask for file name to read and save result to a new text file
 fileName = input('Please enter the name of the template file you wish to use:  ')
 lib = open('{0}/{1}.txt'.format(os.getcwd(), fileName))
 string = lib.read()
+
+#find occurrences
+replace = 0
+madlib = re.compile(r'(NOUN|VERB|ADVERB|ADJECTIVE)')
+matche = madlib.findall(string)
